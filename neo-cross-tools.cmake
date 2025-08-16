9866edfe6c6208d2d9edbbfcac0343ef0996633e
p-a-gridin <p.a.gridin@gmail.com>
2025-08-16 13:21:14 +0300

    + nano pi neo, lyra

---
+++ neo-cross-tools.cmake
@@ -0,0 +1,31 @@
+# sources: 
+#   https://blog.kitware.com/cross-compiling-for-raspberry-pi/
+#   https://gitlab.kitware.com/cmake/community/-/wikis/doc/cmake/CrossCompiling
+
+# this one is important
+SET(CMAKE_SYSTEM_NAME Linux)
+
+#this one not so much
+SET(CMAKE_SYSTEM_VERSION 1)
+
+# specify the cross compiler
+SET(CMAKE_C_COMPILER   C:/SysGCC/raspberry/bin/arm-linux-gnueabihf-gcc.exe)
+SET(CMAKE_CXX_COMPILER C:/SysGCC/raspberry/bin/arm-linux-gnueabihf-g++.exe)
+
+# where is the target environment
+SET(CMAKE_FIND_ROOT_PATH C:/SysGCC/raspberry)
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+set(CMAKE_SYSROOT C:/SysGCC/raspberry/arm-linux-gnueabihf/sysroot)
+
+include_directories(C:/SysGCC/nanopi-neo/include)
+
+# иногда не работает: link_directories(C:/SysGCC/nanopi-neo/lib)
+list(APPEND TBB_LIBS C:/SysGCC/nanopi-neo/lib/libtbb.so C:/SysGCC/nanopi-neo/lib/libtbbmalloc.so)
+
